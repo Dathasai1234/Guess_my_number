db4f96b245d083605c56128e6678778a99d13703
Datha <dathasaichandu@gmail.com>
2022-01-29 12:53:06 +0530

    Commented everything in script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-console.log(document.querySelector('.message').textContent = ' 🎉 Correct Number!');
+// console.log(document.querySelector('.message').textContent = ' 🎉 Correct Number!');
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
