492a344597d39e7c468fe96f02cdfc19b785879c
Datha <dathasaichandu@gmail.com>
2022-01-29 12:50:59 +0530

    Changed some content using DOM manipulation

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 console.log(document.querySelector('.message').textContent);
 
 console.log(document.querySelector('.message').textContent = ' 🎉 Correct Number!');
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
